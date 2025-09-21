d04d14efd717cb3be7c76d7c9bcbe13a375bb5f5
DavidBoni97 <davidbonivardi15@gmail.com>
2025-09-21 20:27:01 -0300

    funcion sorteo de amigo

--- app.js
+++ app.js
@@ -25,3 +25,18 @@ function actualizarLista() {
     ul.appendChild(li);
   }
 }
+
+function sortearAmigo() {
+ 
+  if (listaAmigos.length === 0) {
+    alert("Agrega al menos un amigo antes de sortear.");
+    return;
+  }
+
+  let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+
+  let amigoSorteado = listaAmigos[indiceAleatorio];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li>🎉 El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
