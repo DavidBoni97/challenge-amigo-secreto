0cd746e57dc0d4946482036c71f562a9e4c20fe5
DavidBoni97 <davidbonivardi15@gmail.com>
2025-09-21 20:25:46 -0300

    funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+let listaAmigos = [];
 
+function agregarAmigo() {
+  const input = document.querySelector('#amigo');
+  const nombre = input.value.trim();
+
+  if (!nombre) {
+    alert('Por favor, inserte un nombre.');
+    return;
+  }
+
+  listaAmigos.push(nombre);
+  actualizarLista();    
+  input.value = '';
+  input.focus();
+}
